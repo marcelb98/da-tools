b2c7fa938d3938ec4f9d5c8efb2d04c8876a7f8e
Marcel Beyer <marcel.beyer@it-maker.eu>
2023-07-28 22:21:58 +0200

    init

---
+++ ixp_portspeeds/ixpSpeeds.py
@@ -0,0 +1,114 @@
+#! /usr/bin/env python3
+
+import sys
+import os
+import requests
+import json
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+Path(".cache").mkdir(exist_ok=True)
+
+def help():
+    print('USAGE: ixpSpeeds.py IXPID')
+    print('')
+    print('Fetches the port speeds of all ports at the IXP from PeeringDB.')
+    print('Result is printed as CSV to stdout.')
+    print('')
+    print('IXPID specifies which IXP should be investigated. The ID can be fetched from the PeeringDB-URL after opening the IXP.')
+    print('  For example for DE-CIX Frankfurt the URL is https://www.peeringdb.com/ix/31 and the IXPID is 31.')
+    print('')
+    print('peeringdb.com might throttle your API requests without authentication.')
+    print('Store your API-Key in the environment variable API_KEY to authenticate for the API requests.')
+    print('The tool is also caching the retreived API results. Delete the .cache directory to clear the cache.')
+
+if len(sys.argv) < 2:
+    help()
+    sys.exit(1)
+
+ixpid = sys.argv[1]
+headers = {'Accept': 'application/json'}
+
+if os.environ['API_KEY'] is not None:
+    headers['Authorization'] = "Api-Key "+os.environ['API_KEY']
+
+speeds = {} # speed: counter, …
+
+if os.path.isfile(f".cache/{ixpid}.ix"):
+    # found data in cache
+    with open(f".cache/{ixpid}.ix", 'r') as f:
+        ixps = f.read()
+else:
+    # cache miss
+    ixps = requests.get(f"https://peeringdb.com/api/ix/{ixpid}", headers=headers).text
+    with open(f".cache/{ixpid}.ix", 'w') as f:
+        f.write(ixps)
+ixps = json.loads(ixps)
+
+more_than_10g = 0
+more_than_20g = 0
+more_than_30g = 0
+nbr_count = 0
+if 'data' not in ixps.keys():
+    print(ixps)
+    sys.exit(1)
+for ixp in ixps['data']:
+    for ixlan in ixp['ixlan_set']:
+        ixlanid = ixlan['id']
+
+        if os.path.isfile(f".cache/{ixlanid}.netixlan"):
+            # found data in cache
+            with open(f".cache/{ixlanid}.netixlan", 'r') as f:
+                netixlans_data = f.read()
+
+        else:
+            # cache miss
+            netixlans_data = requests.get(f"https://peeringdb.com/api/netixlan?ixlan={ixlanid}", headers=headers).text
+            with open(f".cache/{ixlanid}.netixlan", 'w') as f:
+                f.write(netixlans_data)
+
+        netixlans = json.loads(netixlans_data)
+
+        for netixlan in netixlans['data']:
+            speed = netixlan['speed']
+            if speed < 10000:
+                # we will group all speeds <10G
+                speed = 9000
+
+            if speed not in speeds:
+                speeds[speed] = 1
+            else:
+                speeds[speed] += 1
+
+            if speed >= 10000:
+                more_than_10g += 1
+            if speed >= 20000:
+                more_than_20g += 1
+            if speed >= 30000:
+                more_than_30g += 1
+            nbr_count += 1
+
+# sort and print speeds
+print(f"Neighbors with >=10G capacity: {(more_than_10g/nbr_count)*100}%")
+print(f"Neighbors with >=20G capacity: {(more_than_20g/nbr_count)*100}%")
+print(f"Neighbors with >=30G capacity: {(more_than_30g/nbr_count)*100}%")
+
+keys = list(speeds.keys())
+keys.sort()
+sorted_speeds = {i: speeds[i] for i in keys}
+
+labels = []
+sizes = []
+for speed in sorted_speeds:
+    sizes.append(sorted_speeds[speed])
+    if speed == 9000:
+        labels.append("<10G")
+    else:
+        labels.append(f"{speed/1000}G")
+
+cm = 1/2.54
+fig, ax = plt.subplots(figsize=(17*cm, 15*cm))
+ax.pie(sizes, labels=labels)
+plt.legend(loc='center left', bbox_to_anchor=(-0.25,0.5))
+plt.show()
+
---
+++ peeringlan_vendors/get_vendors.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python3
+
+import sys
+import json
+from mac_vendor_lookup import MacLookup, VendorNotFoundError, InvalidMacError
+
+def help():
+    print('USAGE: get_vendors.py FORMAT FILE OUTFORMAT')
+    print('')
+    print('Reads the neighbor table from FILE and counts how often a device vendor is listed.')
+    print('Result is printed as CSV to stdout.')
+    print('')
+    print('FORMAT specifies which type of data is provided:')
+    print('  iproute2   Data as returned by executing:')
+    print('  $ ip neigh show | grep -v fe80 | grep REACHABLE')
+    print('   ixf        Data as returned by IXF API call')
+    print('')
+    print('OUTFORMAT specifies in which format the data should be returned.')
+    print('  csv    is the default.')
+    print('  latex  will format the output as a LaTeX tabular.')
+
+if len(sys.argv) < 3:
+    help()
+    sys.exit(1)
+
+format = 'csv'
+if len(sys.argv) > 3 and sys.argv[3] == 'latex':
+    format = 'latex'
+
+mac = MacLookup()
+mac.update_vendors()
+
+vendors = {
+    '_unknown': 0,
+    '_invalid': 0
+}
+
+if sys.argv[1] == 'iproute2':
+    with open(sys.argv[2], 'r') as f:
+        for line in f.readlines():
+            address = line.split(" ")[4]
+            try:
+                v = mac.lookup(address)
+                if v in vendors.keys():
+                    vendors[v] += 1
+                else:
+                    vendors[v] = 1
+            except VendorNotFoundError:
+                vendors['_unknown'] += 1
+            except InvalidMacError:
+                vendors['_invalid'] += 1
+elif sys.argv[1] == 'ixf':
+    with open(sys.argv[2], 'r') as f:
+        data = json.load(f)
+        for member in data['member_list']:
+            member_mac_list = set()
+            for connection in member['connection_list']:
+                for vlan in connection['vlan_list']:
+                    if 'ipv4' in vlan:
+                        for address in vlan['ipv4']['mac_addresses']:
+                            member_mac_list.add(address)
+                    if 'ipv6' in vlan:
+                        for address in vlan['ipv6']['mac_addresses']:
+                            member_mac_list.add(address)
+            for address in member_mac_list:
+                try:
+                    v = mac.lookup(address)
+                    if v in vendors.keys():
+                        vendors[v] += 1
+                    else:
+                        vendors[v] = 1
+                except VendorNotFoundError:
+                    vendors['_unknown'] += 1
+                except InvalidMacError:
+                    vendors['_invalid'] += 1
+
+else:
+    help()
+    sys.exit(1)
+
+if format == 'latex':
+    print('\\begin{table}[p]')
+    print('    \centering')
+    print('    \\begin{tabular}{l l l}')
+    print('        Vendor & Absolute occurrences & relative\\\\')
+    print('        \\hline')
+total = sum(count for vendor, count in vendors.items())
+vendors_sorted = sorted(vendors.items(), key=lambda x:x[1], reverse=True)
+for vendor in vendors_sorted:
+    if format == 'latex':
+        print(f"        {vendor[0]} & {vendor[1]} & {(vendor[1]/total)*100:.2f}\\%\\\\")
+    else:
+        print(f"{vendor[0]},{vendor[1]},{(vendor[1]/total)*100:.2f}%")
+if format == 'latex':
+    print('    \\end{tabular}')
+    print('    \\caption{MAC vendors}')
+    print('    \\label{tab:macVendors}')
+    print('\\end{table}')
+
